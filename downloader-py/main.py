b7ff745214ee55d4fe6866c43aab8919ce6220c9
Almafazi <almafazi@icloud.com>
2025-05-01 01:04:00 +0700

    test debug

--- downloader-py/main.py
+++ downloader-py/main.py
@@ -459,4 +459,4 @@ async def download_slideshow_endpoint(url: str, background_tasks: BackgroundTask
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3029, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=3029, reload=True, debug=True)
