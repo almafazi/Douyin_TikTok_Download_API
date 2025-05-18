f65592bbf49e09c8f3fb3a375e0f5c5898a74fd7
Almafazi <almafazi@icloud.com>
2025-05-18 20:11:02 +0700

    fix

---
+++ gunicorn.conf.py
@@ -0,0 +1,28 @@
+# gunicorn.conf.py
+from app.main import Host_IP, Host_Port
+
+# Jumlah worker berdasarkan CPU
+workers = 8
+
+# Menggunakan Uvicorn worker
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# Binding ke semua interface dengan port 8000
+bind = f"{Host_IP}:{Host_Port}"
+
+# Timeout dalam detik
+timeout = 120
+
+# Log level
+loglevel = "info"
+
+# Preload aplikasi untuk performa lebih baik
+preload_app = True
+
+# Pengaturan log
+accesslog = "-"  # Output ke stdout
+errorlog = "-"   # Output ke stderr
+
+# Jika ingin menyimpan log ke file, gunakan:
+# accesslog = "/var/log/gunicorn/access.log"
+# errorlog = "/var/log/gunicorn/error.log"
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Starting the Python application directly using python3
-python3 start.py
+gunicorn app.main:app -c gunicorn.conf.py
