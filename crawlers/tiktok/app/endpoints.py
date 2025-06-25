69d3ee5cd0bac0b07d82a35592794a65c8539029
Almafazi <almafazi@icloud.com>
2025-06-25 23:05:22 +0700

    rollback without proxy

--- crawlers/tiktok/app/endpoints.py
+++ crawlers/tiktok/app/endpoints.py
@@ -4,8 +4,7 @@ class TikTokAPIEndpoints:
     """
 
     # Tiktok域名 (Tiktok Domain)
-    #TIKTOK_DOMAIN = "https://api22-normal-c-alisg.tiktokv.com"
-    TIKTOK_DOMAIN = "https://snaptik.almafazi1998.workers.dev"
+    TIKTOK_DOMAIN = "https://api22-normal-c-alisg.tiktokv.com"
 
     # 视频主页Feed (Home Feed)
     HOME_FEED = f"{TIKTOK_DOMAIN}/aweme/v1/feed/"
